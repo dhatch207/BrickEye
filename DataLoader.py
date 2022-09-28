cb51bb9dd568d78ba67c5425336c754464e331ff
Dominic Hatch <dmh207@gmail.com>
2022-09-27 19:33:14 -0500

    add visualizer file

--- DataLoader.py
+++ DataLoader.py
@@ -1,24 +1,20 @@
-from operator import irshift
-from time import time
-from typing import *
-from numpy import piecewise
 import pandas as pd
-import os
 import subprocess
-from functools import reduce
-from os.path import exists
+import os
 
 class DataLoader():
 
     def load_set_price_guide(self, data_directory="../DATA", set_id="75902-1",  exclude_incomplete=True):
-        path = os.path.join(os.path.dirname(__file__), f"{data_directory}/SETS/{set_id}{'C' if exclude_incomplete else 'CI'}")
+        path = os.path.join(os.path.dirname(__file__), f"{data_directory}/SETS/{set_id}-{'C' if exclude_incomplete else 'CI'}")
         url = f"https://www.bricklink.com/catalogPG.asp?S={set_id}&colorID=0&v=D&viewExclude={'Y' if exclude_incomplete else 'N'}&cID=Y"
         return self._get_price_guide_data(path, url)
 
-    def load_part_price_guide(self, data_directory="../DATA",part_number='62113',page_color='11',price_and_quantity_color='85'):
-        path = os.path.join(os.path.dirname(__file__), f"{data_directory}/PARTS/{part_number}{price_and_quantity_color}")
-        url = f"https://www.bricklink.com/v2/catalog/catalogitem.page?P={part_number}&idColor={page_color}#T=P&C={price_and_quantity_color}"
-        return self._get_price_guide_data(path, url)
+    # wasnt downloading right, skipping for now
+    #def load_part_price_guide(self, data_directory="../DATA", part_id='21042pb01c02', color_id='150'):
+    #    path = os.path.join(os.path.dirname(__file__), f"{data_directory}/PARTS/{part_id}-{color_id}")
+    #    url = f"https://www.bricklink.com/catalogPG.asp?P={part_id}&ColorID={color_id}"
+    #   url = "https://www.bricklink.com/catalogPG.asp?P=21042pb01c02&colorID=150&viewExclude=N&v=D&Y"
+     #   return self._get_price_guide_data(path, url)
 
     def _get_price_guide_data(self, path, url):
         """
@@ -26,16 +22,15 @@ class DataLoader():
         @param url: url of bricklink price guide of item
         @param path: relative path to where data is stored / will be stored 
         """
-        if not exists(path):
+        if not os.path.exists(path):
             _status, output = subprocess.getstatusoutput(f"curl {url}")
             with open(path, "w+") as file:
                 file.write(output)
             
         with open(path, "r") as file:
-            data = pd.read_html(file.read())
+            data = file.read()
 
         return self._parse_data(data)
-        
 
     def _parse_data(self, data):
         """
@@ -44,13 +39,18 @@ class DataLoader():
         @returns history, book
         """
 
-        if (len(data[19:-7]) % 3):
+        df = pd.read_html(data)
+
+        #for table, i in enumerate(df):
+        #    print(f"{i}.................................: \n\n{table}\n")
+
+        if (len(df[19:-7]) % 3):
             print("\n\nBAD DATA!!!\n\n")
             
         history = pd.DataFrame()
         min_date = pd.Timestamp.today()
         new = True
-        for i, item in enumerate(data[19:-7]):
+        for i, item in enumerate(df[19:-7]):
             if i % 3 == 0:
                 date = pd.to_datetime(item.iloc[0,0])
                 if date > min_date:
@@ -62,15 +62,18 @@ class DataLoader():
                 item['Date'] = [date for x in item.index]
                 item['New'] = [new for x in item.index]
                 history = pd.concat([history, item])
+        history = history.astype({'Qty': 'int', 'Each': 'string'})
+        history['Each'] = history['Each'].apply(lambda x: x[x.find("$")+1:])
+        history = history.astype({'Each': 'float'})
+        history = history.sort_values(by='Date')
 
         book = pd.DataFrame()
-        for i, item in enumerate(data[-7:-1]):
+        for i, item in enumerate(df[-7:-1]):
             new = i % 6 == 2
             if i % 3 == 2:
                 item.columns = item.iloc[0]
                 item = item.iloc[1:-7,1:]
                 item['New'] = [new for x in item.index]
-                print(item)
                 book = pd.concat((book, item))
 
         return (history, book)
@@ -78,7 +81,7 @@ class DataLoader():
 def main():
     data_loader = DataLoader()
     print(data_loader.load_set_price_guide())
-    print(data_loader.load_part_price_guide())
+    #print(data_loader.load_part_price_guide())
 
 if (__name__ == '__main__'):
     main()
---
+++ Visualizer.py
@@ -0,0 +1,39 @@
+from turtle import color, home
+
+from numpy import size, mean
+from DataLoader import DataLoader
+import matplotlib.pyplot as plt
+
+def plot_historical_transactions(history):
+    df = history.copy()
+    df = df.iloc[1:,:] # drop outlier
+    df['color'] = df['New'].apply(lambda x: 'tab:blue' if x else 'tab:orange')
+    
+    plt.scatter(x=df['Date'], y=df['Each'], color=df['color'], s=df['Qty'] * 100)
+
+    plt.show()
+
+def get_average_six_month_sale_price(history):
+    df = history.copy()
+    df = df.iloc[1:,:] # drop outlier
+    new = mean(df[df['New']]['Each'])
+    used = mean(df[~df['New']]['Each'])
+    return new, used
+
+
+def main():
+    data_loader = DataLoader()
+    total_new, total_used = 0, 0
+    #for set_id in ["10220-1", "75902-1", "75904-1", "10181-1"]:#, "5526-1"]:
+    for set_id in ["75181-1", "75053-1", "75180-1", "7868-1", "7675-1", "7259-1", "8095-1",  "8096-1", "75176-1", "8098-1"]: #"4124171-1",
+        history, book = data_loader.load_set_price_guide(set_id=set_id)
+        #plot_historical_transactions(history)
+        new, used = get_average_six_month_sale_price(history)
+        total_new += new
+        total_used += used
+        print(f"set: {set_id} used: {used} new: {new}")
+    print(f"total used: {total_used} new: {total_new}")
+        
+
+if (__name__ == '__main__'):
+    main()
